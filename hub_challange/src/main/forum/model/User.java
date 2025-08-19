4dc4a6a9be4bbfa6eb86b765ae52c580f04d3327
Gabriel Silva <gabriellrezzendee@gmail.com>
2025-08-19 19:38:23 -0300

    Create User.java

---
+++ hub_challange/src/main/forum/model/User.java
@@ -0,0 +1,29 @@
+package com.alura.forumhub.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+}
